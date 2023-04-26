550da35a4978733df3809afa4d643d6a86a5fa49
fklimczak <131850120+fklimczak@users.noreply.github.com>
2023-04-26 08:37:21 +0200

    Add files via upload

---
+++ garaz.py
@@ -0,0 +1,122 @@
+# def kwadrat(liczba):
+#     return liczba**2
+
+# print("Wpisz cyfre")
+# liczba = int(input())
+# wynik = kwadrat(liczba)
+
+# print(wynik)
+
+# def dodawanie(licz1, licz2):
+#     suma = licz1 + licz2
+#     return suma
+# def mnozenie(licz1, licz2):
+#     iloczyn = licz1*licz2
+#     return iloczyn
+
+# print("Podaj liczbe")
+# pierwsza = int(input())
+# print("Podaj 2 liczbe")
+# druga = int(input())
+# suma1 = dodawanie(pierwsza, druga)
+# suma2 = mnozenie(pierwsza, druga)
+# print(suma1)
+# print(suma2)
+
+# def dziwne_dzialanie(liczba1):
+#     suma = liczba1+liczba1
+#     kwadrat = suma**2
+#     wynik = kwadrat - 1
+#     return wynik
+
+# print("Podaj liczbe")
+# pierwsza = float(input())
+# wynik = dziwne_dzialanie(pierwsza)
+# print("Wynikiem działania jest: ", wynik)
+# print("Kwadratem podanych liczb jest: ")
+
+# def dlugosc(lista):
+#     pass
+#     suma = 0
+#     for element in lista:
+#             suma = suma+1
+#     return suma
+
+# def maksimum(lista):
+#     najwieksza = lista[0]
+#     for element in lista:
+#         if element > najwieksza:
+#             najwieksza = element
+#     return najwieksza
+
+# def minimum(lista):
+#     najmniejsza = lista[0]
+#     for element in lista:
+#         if element < najmniejsza:
+#             najmniejsza = element
+#     return najmniejsza
+
+# def ostatni(lista):
+#     last = lista[-1]
+#     return last
+
+
+# def ogon(lista):
+#     caly_ogon = lista.remove(lista[-1])
+#     return caly_ogon
+
+def koniunkcja(p, q):
+        return p*q
+def alternatywa(p, q):
+    if p+q>1:
+        return 1
+    else:
+        return p+q
+
+def implikacja(p, q):
+    if p == 1 and q == 0:
+        return 0
+    else:
+        return 1
+
+def calendar(lista):
+    
+
+
+
+
+
+
+lista_dni = ["poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela"]
+
+
+# print("Wpisz p")
+# p = int(input())
+# print("Wpisz q")
+# q = int(input())
+# wynik_implikacji = implikacja(p, q)
+# print("Wynik implikacji to: ", wynik_implikacji)
+
+
+
+
+# lista = [43433,34342,65677,543,3123,5467,978]
+
+# leng = dlugosc(lista)
+# max_listy = maksimum(lista)
+# min_listy = minimum(lista)
+# last_element = ostatni(lista)
+
+# kon = koniunkcja(p, q)
+
+# print(leng)
+# print(max_listy)
+# print(min_listy)
+# print(last_element)
+
+# print(kon)
+
+
+
+    
+
